3e71a60252d9b3a143b950be1f1f600cece7840e
Calvin Howard <calvin.uhh@gmail.com>
2026-07-24 23:25:26 -0500

    fix/ corrigiendo cliente de postgres para produccion

--- website/lib/db/client.ts
+++ website/lib/db/client.ts
@@ -1,12 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set');
-}
-
-export const db = drizzle(connectionString);
-
+export const db = drizzle({ client: pool });
 export { schema };
